a2b1f038c16c96a991acce189cce84bc92826ada
nevermin9 <chizzza@gmail.com>
2025-09-29 12:32:30 +0200

    feat: install PHPMailer; add mailhog to docker compose for mail testing;

--- app/public/index.php
+++ app/public/index.php
@@ -15,12 +15,37 @@ use App\Controllers\LoginController;
 use App\Controllers\RegistrationController;
 use App\Controllers\StackOverviewController;
 
+
+
+
+use App\Core\View;
+use App\Services\MailService;
+
 define('VIEWS_DIR', __DIR__ . "/../src/Views");
 
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 $router = new Router();
+
+class TestMail
+{
+    public function index()
+    {
+        return View::make("test-mail");
+    }
+
+    public function handle()
+    {
+        $mail = new MailService();
+        $mail->setRecipients(
+            ['address' => 'fooo@gmail.com', 'name' => 'Your Daddy'],
+            ['address' => $_POST['address'], $_POST['name']]
+        );
+        $mail->fillHTML('Subject', 'Click <a href="google.com">here</a>;');
+        $mail->send();
+    }
+}
 
 $router
     ->get("/", [HomeController::class, "index"])
@@ -31,7 +56,9 @@ $router
     ->get("/stack/:id", [StackOverviewController::class, "index"])
     ->get("/logout", [LoginController::class, "logout"])
     ->post("/stack/create", [HomeController::class, "createStack"])
-    ->post("/stack/:stackId/add-flashcard", [StackOverviewController::class, "addFleshcard"]);
+    ->post("/stack/:stackId/add-flashcard", [StackOverviewController::class, "addFleshcard"])
+    ->get("/test-email", [TestMail::class, "index"])
+    ->post("/test-email", [TestMail::class, "handle"]);
 
 
 new Application($router, new Config($_ENV))->run();

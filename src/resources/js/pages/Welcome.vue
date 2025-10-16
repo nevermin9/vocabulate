b3e42790f54146c293ef3623061daeed20aac27b
nevermin9 <chizzza@gmail.com>
2025-10-16 18:58:25 +0200

    migrate laravel app to yarn

--- src/resources/js/pages/Welcome.vue
+++ src/resources/js/pages/Welcome.vue
@@ -27,7 +27,7 @@ import { Head, Link } from '@inertiajs/vue3';
                         :href="login()"
                         class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
                     >
-                        Log in
+                    login
                     </Link>
                     <Link
                         :href="register()"

b9037874ab458f2947468a2a986efd7050026638
nevermin9 <chizzza@gmail.com>
2025-11-05 13:03:35 +0100

    protect main page from anonymous;

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,10 +5,13 @@ use Illuminate\Container\Attributes\CurrentUser;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', fn () => Inertia::render('Welcome'))->name('home');
+Route::get('/', fn () => 'Hello, World!')
+    ->middleware(['auth'])
+    ->name('home');
 
 Route::get('dashboard', fn () => Inertia::render('Dashboard'))
-    ->middleware(['auth', 'verified'])->name('dashboard');
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';

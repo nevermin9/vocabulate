753e14993bb5bef816fa703147ceff3939d16091
nevermin9 <chizzza@gmail.com>
2025-11-05 10:59:32 +0100

    formatting

--- src/resources/js/layouts/auth/AuthSimpleLayout.vue
+++ src/resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -29,8 +29,10 @@ defineProps<{
                         </div>
                         <span class="sr-only">{{ title }}</span>
                     </Link>
+
                     <div class="space-y-2 text-center">
                         <h1 class="text-xl font-medium">{{ title }}</h1>
+
                         <p class="text-center text-sm text-muted-foreground">
                             {{ description }}
                         </p>

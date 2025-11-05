042e99e853e7b2909ce72cc17e68e990b8e48aa6
nevermin9 <chizzza@gmail.com>
2025-11-05 17:20:05 +0100

    migrage inputs styling

--- src/resources/js/components/ui/input/Input.vue
+++ src/resources/js/components/ui/input/Input.vue
@@ -4,30 +4,37 @@ import { cn } from '@/lib/utils'
 import { useVModel } from '@vueuse/core'
 
 const props = defineProps<{
-  defaultValue?: string | number
-  modelValue?: string | number
-  class?: HTMLAttributes['class']
+    defaultValue?: string | number
+    modelValue?: string | number
+    class?: HTMLAttributes['class']
 }>()
 
 const emits = defineEmits<{
-  (e: 'update:modelValue', payload: string | number): void
+    (e: 'update:modelValue', payload: string | number): void
 }>()
 
 const modelValue = useVModel(props, 'modelValue', emits, {
-  passive: true,
-  defaultValue: props.defaultValue,
+    passive: true,
+    defaultValue: props.defaultValue,
 })
 </script>
 
 <template>
-  <input
-    v-model="modelValue"
-    data-slot="input"
-    :class="cn(
-      'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-      'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-      'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-      props.class,
-    )"
-  >
+    <input
+        v-model="modelValue"
+        data-slot="input"
+        :class="cn(
+            'flex h-10 w-full min-w-0 px-3 py-1',
+            'rounded-lg border-2 border-solid border-transparent focus-visible:not-user-invalid:not-aria-invalid:border-border',
+            'outline outline-solid outline-ring hover:not-user-invalid:not-aria-invalid:outline-ring-active focus-visible:not-user-invalid:not-aria-invalid:outline-transparent',
+            'bg-input',
+            'text-base md:text-sm',
+            'transition-all transition-200',
+            'aria-invalid:not-focus-visible:outline-destructive aria-invalid:focus-visible:outline-transparent aria-invalid:focus-visible:border-destructive',
+            'file:text-foreground file:inline-flex file:h-9 file:border-0 file:bg-transparent file:text-sm file:font-medium',
+            'disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-30',
+            'placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground',
+            props.class,
+        )"
+    >
 </template>

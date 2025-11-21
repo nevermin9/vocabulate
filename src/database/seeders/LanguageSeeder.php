046dde809bba891613875618f3258e16bed19bf5
nevermin9 <chizzza@gmail.com>
2025-11-21 13:23:36 +0100

    create LanguageSeeder; seed languages table;

--- src/database/seeders/LanguageSeeder.php
+++ src/database/seeders/LanguageSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LanguageSeeder extends Seeder
 {
@@ -12,6 +13,27 @@ class LanguageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('languages')->insert([
+            ['code' => 'cz', 'name' => 'Czech'],
+            ['code' => 'el', 'name' => 'Greek'],
+            ['code' => 'en', 'name' => 'English'],
+            ['code' => 'es', 'name' => 'Spanish'],
+            ['code' => 'et', 'name' => 'Estonian'],
+            ['code' => 'fr', 'name' => 'French'],
+            ['code' => 'it', 'name' => 'Italian'],
+            ['code' => 'ja', 'name' => 'Japanese'],
+            ['code' => 'ko', 'name' => 'Korean'],
+            ['code' => 'lt', 'name' => 'Lithuanian'],
+            ['code' => 'lv', 'name' => 'Latvian'],
+            ['code' => 'nl', 'name' => 'Dutch'],
+            ['code' => 'no', 'name' => 'Norwegian'],
+            ['code' => 'pl', 'name' => 'Polish'],
+            ['code' => 'pt', 'name' => 'Portuguese'],
+            ['code' => 'ro', 'name' => 'Romanian'],
+            ['code' => 'sv', 'name' => 'Swedish'],
+            ['code' => 'tr', 'name' => 'Turkish'],
+            ['code' => 'uk', 'name' => 'Ukranian'],
+            ['code' => 'zh', 'name' => 'Chinese'],
+        ]);
     }
 }
